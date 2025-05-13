7b3eb318a1d0817e73cf76da1ffe7632a147f203
dinhtranvietanh <dinhtranvietanh253@gmail.com>
2025-05-13 14:26:24 +0700

    Initial Commit

---
+++ loggin/logginApp.swift
@@ -0,0 +1,17 @@
+//
+//  logginApp.swift
+//  loggin
+//
+//  Created by Đinh Trần Việt Anh on 13/5/25.
+//
+
+import SwiftUI
+
+@main
+struct logginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ logginTests/logginTests.swift
@@ -0,0 +1,17 @@
+//
+//  logginTests.swift
+//  logginTests
+//
+//  Created by Đinh Trần Việt Anh on 13/5/25.
+//
+
+import Testing
+@testable import loggin
+
+struct logginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
