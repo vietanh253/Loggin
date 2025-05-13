d65e02f9a90a42ba0270a5dca7d798a371e383cf
dinhtranvietanh <dinhtranvietanh253@gmail.com>
2025-05-13 16:58:28 +0700

    set up facebook loggin

--- New Group/New Group/New Group/loggin/Helper/AppDelegate.swift
+++ New Group/New Group/New Group/loggin/Helper/AppDelegate.swift
@@ -1,18 +1,19 @@
 import SwiftUI
 import FirebaseCore
 import GoogleSignIn
+import FacebookCore
 
 class AppDelegate : NSObject , UIApplicationDelegate {
   func application ( _ application : UIApplication ,
                    didFinishLaunchingWithOptions launchOptions : [ UIApplication . LaunchOptionsKey : Any ]? = nil ) -> Bool {
     FirebaseApp.configure ( )
-
+      ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     return true
   }
     
     func application(_ app: UIApplication,
                      open url: URL,
                      options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-      return GIDSignIn.sharedInstance.handle(url)
+        return ApplicationDelegate.shared.application(app,open: url,sourceApplication: options[.sourceApplication] as? String, annotation: options[.annotation])
     }
 }
--- New Group/New Group/New Group/loggin/logginApp.swift
+++ New Group/New Group/New Group/loggin/logginApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+//import FacebookCore
 
 @main
 struct logginApp: App {
